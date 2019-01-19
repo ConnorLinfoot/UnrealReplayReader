af112b9e517c67b5b1e5f6ba5785a15a84bc6f8d
Connor Linfoot <connorlinfoot@me.com>
2019-01-19 17:58:53 +0000

    Include weaponId

--- src/main/java/io/erosemberg/reader/gamedata/fortnite/FortniteGameData.java
+++ src/main/java/io/erosemberg/reader/gamedata/fortnite/FortniteGameData.java
@@ -43,6 +43,7 @@ public class FortniteGameData implements GameData {
     public static class Kill {
         String killer;
         String killed;
+        long weaponId;
         FortniteWeaponTypes type;
 
         boolean isElimination;
